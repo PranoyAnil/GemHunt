ad53e9b23ee5a81f5d67bef3ee510197862a75dc
PranoyAnil <pranoy.anil@gmail.com>
2016-02-28 02:07:42 -0500

    Created player

---
+++ Assets/_Scripts/SpikedWheelController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpikedWheelController : MonoBehaviour {
+	// PRIVATE INSTANCE VARIABLES
+	private Transform _transform;
+
+
+	// Use this for initialization
+	void Start () {
+		this._transform = gameObject.GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this._transform.Rotate (0, 0, -3f);
+
+	}
+}
